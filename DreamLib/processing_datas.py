8d608cdc61c119782d2b0d56c096d2db6279439f
lilhugo <hugo.fourel@yahoo.com>
2022-12-12 15:41:07 +0100

    datas

--- DreamLib/processing_datas.py
+++ DreamLib/processing_datas.py
@@ -30,11 +30,11 @@ def clean_data(data:pd.DataFrame,start:int,period:int,chronique,col_used:None,sp
   X = X.set_index(X.TRIMESTRE)
   X["DR"] = X["DR"].shift(-period//3)
   Y = X.DR.iloc[:-period//3]
+  X = X.drop(columns=["DR","TRIMESTRE","CHRONIQUE"])
   if col_used != None:
     X = X[col_used]
   else:
       X = X.drop(columns=['CD_TY_CLI_RCI_2','CD_ETA_CIV_2','CD_MOD_HABI_2','CD_PROF_3','CD_QUAL_VEH_2'])
-  X = X.drop(columns=["DR","TRIMESTRE","CHRONIQUE"])
   X_validation = X.iloc[-period//3:,:]
   X = X.iloc[:-period//3,:]
 
@@ -46,6 +46,7 @@ def clean_data(data:pd.DataFrame,start:int,period:int,chronique,col_used:None,sp
     X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=split,shuffle=False)
   index_train = X_train.index
   index_test = X_test.index
+  index_validation = X_validation.index
 
   # Normalization
   if norm == "Not":
@@ -60,7 +61,7 @@ def clean_data(data:pd.DataFrame,start:int,period:int,chronique,col_used:None,sp
     scaler_validation = MinMaxScaler()
     X_train = pd.DataFrame(scaler_train.fit_transform(X_train),index=index_train,columns=X_train.columns)
     X_test = pd.DataFrame(scaler_test.fit_transform(X_test),index=index_test,columns=X_test.columns)
-    X_validation = pd.DataFrame(scaler_validation.fit_transform(X_validation),index=index_test,columns=X_validation.columns)
+    X_validation = pd.DataFrame(scaler_validation.fit_transform(X_validation),index=index_validation,columns=X_validation.columns)
 
   elif norm == 'StdSca':
     scaler_train = StandardScaler()
@@ -68,7 +69,7 @@ def clean_data(data:pd.DataFrame,start:int,period:int,chronique,col_used:None,sp
     scaler_validation = StandardScaler()
     X_train = pd.DataFrame(scaler_train.fit_transform(X_train),index=index_train,columns=X_train.columns)
     X_test = pd.DataFrame(scaler_test.fit_transform(X_test),index=index_test,columns=X_test.columns)
-    X_validation = pd.DataFrame(scaler_validation.fit_transform(X_validation),index=index_test,columns=X_validation.columns)
+    X_validation = pd.DataFrame(scaler_validation.fit_transform(X_validation),index=index_validation,columns=X_validation.columns)
   
   else:
     print("This norm doesn't exist")
